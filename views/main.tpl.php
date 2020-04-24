5e38382da1603ffed0220b79e07e1b4c6fa465b0
root <root@HomeIron>
2020-04-24 14:12:51 +0300

    первая итерация главной вьюхи

--- views/main.tpl.php
+++ views/main.tpl.php
@@ -2,12 +2,17 @@
  <html lang="en">
  <head>
  	<meta charset="UTF-8">
- 	<title></title>
+ 	<title><?php echo $pageData['title']; ?></title>
  	<meta name="viewport" content="width=device-width, initial-scale=1">
  	<link rel="stylesheet" type="text/css" href="/css/bootstrap.min.css">
  	<link rel="stylesheet" type="text/css" href="/css/style.css">
  </head>
  <body>
+
+	<div class="container text-center">
+        <h1>MAIN.TPL.PHP</h1>
+        <p class="lead">основная вьюха</p>
+    </div>
  	
 	<script type="text/javascript" src="/js/bootstrap.min.js"></script>
 
